63d5930ce90b8316634c41f1f6bdb67706cefb72
nerdgamer2662 <vijay.shastri2602@gmail.com>
2024-09-15 10:16:20 -0400

    Springboot generated files
    Adds all the files and folders that Springboot generates as a part of a project, including the recommended frameworks

---
+++ locationsearch/src/test/java/com/cs3300/group8/locationsearch/LocationsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs3300.group8.locationsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
