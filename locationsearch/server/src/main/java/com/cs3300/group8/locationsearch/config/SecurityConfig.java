d9e7529527c39675fecb0500f33f0916d93500c3
JIAHAO CHEN <100472232@alumnos.uc3m.es>
2024-10-01 17:46:22 -0400

    cleaning application properties

--- locationsearch/server/src/main/java/com/cs3300/group8/locationsearch/config/SecurityConfig.java
+++ locationsearch/server/src/main/java/com/cs3300/group8/locationsearch/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,8 +16,11 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/api/users/signup", "/api/users/login").permitAll()
+                .requestMatchers("/api/users/signup", "/api/users/login",  "/oauth2/**").permitAll()
                 .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2 -> oauth2
+                .defaultSuccessUrl("/your-success-url") // Customize this based on your app
             )
             .httpBasic(httpBasic -> {});
         
