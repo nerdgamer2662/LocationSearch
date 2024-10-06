f61d524dfbdded206d7320d2a4b2c5bdbd49336e
nerdgamer2662 <vijay.shastri2602@gmail.com>
2024-10-06 17:14:06 -0400

    Alters prompt to fit new goal
    The goal is now to fit a broad spectrum of search terms into a narrow set of categories. The prompt has been adjusted to fit this.

--- locationsearch/client/src/components/GPTRequest.js
+++ locationsearch/client/src/components/GPTRequest.js
@@ -5,32 +5,34 @@ export async function getSynonyms(initWord) {
     let completion = await openai.chat.completions.create({
         model: "gpt-4o-mini",
         messages: [
-            {"role": "user", "content": "Query: Provide examples of synonyms for a given location.\nExample 1: Some synonyms for \"restaurant\" are: cafeteria, diner, eatery\nExample 2: Some synonyms for \"" + initWord + "\" are:"}
+            {"role": "user", "content": "Query: Fit a given word to one of the following categories: accounting,airport,amusement_park,aquarium,art_gallery,atm,bakery,bank,bar,beauty_salon,bicycle_store,book_store,bowling_alley,bus_station,cafe,campground,car_dealer,car_rental,car_repair,car_wash,casino,cemetery,church,city_hall,clothing_store,convenience_store,courthouse,dentist,department_store,doctor,drugstore,electrician,electronics_store,embassy,fire_station,florist,funeral_home,furniture_store,gas_station,gym,hair_care,hardware_store,hindu_temple,home_goods_store,hospital,insurance_agency,jewelry_store,laundry,lawyer,library,light_rail_station,liquor_store,local_government_office,locksmith,lodging,meal_delivery,meal_takeaway,mosque,movie_rental,movie_theater,moving_company,museum,night_club,painter,park,parking,pet_store,pharmacy,physiotherapist,plumber,police,post_office,primary_school,real_estate_agency,restaurant,roofing_contractor,rv_park,school,secondary_school,shoe_store,shopping_mall,spa,stadium,storage,store,subway_station,supermarket,synagogue,taxi_stand,tourist_attraction,train_station,transit_station,travel_agency,university,veterinary_care,zoo\n" + 
+                                "Example 1: An inn is in the category of: hotel\n" + "Example 2: A(n) " + 
+                                initword.trim() + " is in the category of:"}
         ]
     });
-    completion = cleanResponse(completion);
+    completion = completion.trim().toLowerCase();
 
-    if (completion) {
-        return completion;
+    if (completion == "none") {
+        throw new Error("Failed to fit to category!");
     } else {
-        throw new Error("Invalid place for finding synonyms!");
+        return completion;
     }
 }
 
-function cleanResponse(res) {
-    if (res.indexOf(":") == -1) {
-        return null;
-    }
-    items = res.substr(res.indexOf(":") + 1).split(", "); // cuts after initial colon
-    let cleanItems = [];
-    items.forEach(element => {
-        cleanItems.push(element
-            .trim() // removes initial whitespace
-            .replace(/\((.*)\)/gm, "") // removes parentheticals
-            .replace(/(?:^|\W)and(?:$|\W)/gm, "") // removes the word and
-            .replace(".", "") // removes periods
-            .trim()); // removes any lingering whitespace
-    });
+// function cleanResponse(res) {
+//     if (res.indexOf(":") == -1) {
+//         return null;
+//     }
+//     items = res.substr(res.indexOf(":") + 1).split(", "); // cuts after initial colon
+//     let cleanItems = [];
+//     items.forEach(element => {
+//         cleanItems.push(element
+//             .trim() // removes initial whitespace
+//             .replace(/\((.*)\)/gm, "") // removes parentheticals
+//             .replace(/(?:^|\W)and(?:$|\W)/gm, "") // removes the word and
+//             .replace(".", "") // removes periods
+//             .trim()); // removes any lingering whitespace
+//     });
 
-    return cleanItems;
-}
+//     return cleanItems;
+// }
